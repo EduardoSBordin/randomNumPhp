8ad56fd774b3ebc371afb8818f07f8908857d345
Eduardo Bordin <dudubordin26@gmail.com>
2023-10-27 21:58:47 -0300

    firstTest

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Just For Test</title>
+</head>
+<body>
+    <h1>Here it's Ok!</h1>
+
+    <h2>
+    <?php 
+
+        $name = "Eduardo";
+        echo "$name";
+    ?>
+    </h2>
+</body>
+</html>
